829eb767690ac25352d1d1086af57a6e01f88596
Shaan Sadanand <cheenu@Shaans-MacBook-Air.local>
2023-08-09 16:56:28 +0930

    Implemented even.c

---
+++ even.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void sigHandlerHUP(int signum){
+
+  printf("Ouch!\n");
+}
+void sigHandlerINT(int signum){
+
+  printf("Yeah!\n");
+}
+
+
+int main(int argc, char* argv[]) {
+    int even = 0;
+    int n = atoi(argv[1]);
+
+    signal(SIGHUP, sigHandlerHUP);
+    signal(SIGINT, sigHandlerINT);
+
+    
+    for (int i = 0; i < n; i++){
+        printf("%d\n", even);
+        even +=2;
+        sleep(5);
+    }
+    return 0;
+}
